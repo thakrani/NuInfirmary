49dfb1e4ced7845f5bc0ed5bd2f4bdd1a1f25cf4
Shivam Thakrani <thakranishivam@gmail.com>
2020-12-09 14:56:05 +0530

    Removed MONGODB Key commit

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    MongoURI: "mongodb+srv://infimry:user123@cluster0.st05f.mongodb.net/?retryWrites=true&w=majority"
-}
-
-// module.exports = {
-//     MongoURI :"mongodb+srv://pratikvispute:pratikvispute@cluster0-jmuwo.mongodb.net/?retryWrites=true&w=majority"
-// }
